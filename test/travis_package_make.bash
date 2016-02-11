0e145324305c1e74073bf6a98fc64a7e2d7b23eb
yasu ekimoto <eki.party1@gmail.com>
2016-02-11 16:46:03 +0000

    Add a file

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,10 +1,23 @@
 #!/bin/bash -xve
 
-#sync and make
+
+
+#ワークスペースにリポジトリをコピー
+
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
+
+
+#pimouse_rosをgit cloneでワークスペースに持ってくる
+
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/eki1eki1/pimouse_ros.git
+
+git clone --depth=1 https://github.com/citueda/pimouse_ros.git
+
+        #↑depth=1を指定すると最新のものだけクローンできる
+
+
 
 cd ~/catkin_ws
+
 catkin_make
